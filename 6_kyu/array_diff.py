36128be2c5c8aa3a12d6a6d80ee5c51c7defb68f
c0mbined <74930812+c0mbined@users.noreply.github.com>
2021-12-11 21:58:15 +0000

    Update array_diff.py

--- 6_kyu/array_diff.py
+++ 6_kyu/array_diff.py
@@ -2,11 +2,8 @@
 # https://www.codewars.com/kata/523f5d21c841566fde000009/python
 
 def array_diff(a, b):
-    occurences = []
+    l = []
     for i in a:
         if i not in b:
-            occurences.append(i)
-    return occurences
-
-
-print(array_diff([1, 2], [1]))
+            l.append(i)
+    return l
